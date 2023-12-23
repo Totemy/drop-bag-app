8130cf51359aabb16a9c8a9b376f8dfee52e416c
Totemy <thetotemofficial@gmail.com>
2023-12-23 12:31:30 +0200

    Edited styles for cart

--- src/views/CartComponent.vue
+++ src/views/CartComponent.vue
@@ -1,43 +1,57 @@
 <template>
-  <div class=" cart-count ">
-    <div class="cart-count__article">
-      <h3>Ваші покупки</h3>
-      <div class="cart-count__list" >
-        <ul v-for="(item, index) in cartItems" :key="index">
-          <li>
-            {{ index + 1 }} - {{ item.name }}
-            <button class="btn btn__delete" @click="removeFromCart(index)">X</button>
-          </li>
-        </ul>
-      </div>
+    <div class="cart-count">
+        <div class="cart-count__content">
+            <div class="cart-count__article">
+                <h3>Ваші покупки</h3>
+                <div class="cart-count__list">
+                    <ul v-for="(item, index) in cartItems" :key="index">
+                        <li>
+                            {{ index + 1 }} - {{ item.name }}
+                            <button
+                                class="btn btn__delete"
+                                @click="removeFromCart(index)"
+                            >
+                                X
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="cart-count__buttons">
+                <button class="btn btn__close" @click="closeCart">
+                    Закрити
+                </button>
+                <button class="btn btn__clear" @click="clearCart">
+                    Очистити корзину
+                </button>
+                <router-link to="/checkout"
+                    ><button class="btn btn__offer">
+                        Оформити замовлення
+                    </button></router-link
+                >
+            </div>
+        </div>
     </div>
-    <div class="cart-count__buttons">
-      <button class="btn btn__close" @click="closeCart">Закрити</button>
-      <button class="btn btn__clear" @click="clearCart">Очистити корзину</button>
-      <router-link to="/checkout"><button class="btn btn__offer" >Оформити замовлення</button></router-link>
-      
-    </div>
-  </div>
 </template>
 <script>
-import { mapGetters } from 'vuex';
-export default{
-  computed: {
-    ...mapGetters('cart', ['cartItems']),
-    cartItems() {
-      return this.$store.state.cart.cartItems;
+import { mapGetters } from 'vuex'
+export default {
+    computed: {
+        ...mapGetters('cart', ['cartItems']),
+        cartItems() {
+            return this.$store.state.cart.cartItems
+        },
     },
-  },
-  methods: {
-    closeCart() {
-      this.$emit('close');
+    methods: {
+        closeCart() {
+            this.$emit('close')
+        },
+        clearCart() {
+            this.$store.commit('cart/clearCart')
+        },
+        removeFromCart(index) {
+            this.$store.commit('cart/removeFromCart', index)
+        },
     },
-    clearCart(){
-      this.$store.commit('cart/clearCart');
-    },
-    removeFromCart(index) {
-      this.$store.commit('cart/removeFromCart', index);
-    },
-  },
 }
 </script>
