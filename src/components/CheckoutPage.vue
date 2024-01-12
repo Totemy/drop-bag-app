933ebaa0cd4bf4a87fbe80d04047728a627627ce
Totemy <thetotemofficial@gmail.com>
2024-01-12 21:33:28 +0200

    Added component to CheckoutPage.vue

--- src/components/CheckoutPage.vue
+++ src/components/CheckoutPage.vue
@@ -3,40 +3,62 @@
         <div>
             <h2>Оформлення замовлення</h2>
             <div class="checkout__personal">
-                <h4>Ваші контактні дані</h4>
-                <input class="checkout__input" type="text" placeholder="Ім'я" />
+                <h4>Прізвище ім'я по батькові</h4>
+                <input class="checkout__input" type="text" placeholder="ПІБ" :value="name" />
+                <h4>
+                    Номер телефону
+                </h4>
                 <input
+                    id="phoneNumber"
                     class="checkout__input"
-                    type="text"
-                    placeholder="Прізвище"
+                    type="tel"
+                    :value="formattedPhoneNumber"
+                    @input="handleInput"
+                    @keydown="handleKeyDown"
                 />
+                <h4>Місто проживання</h4>
+                <input class="checkout__input" type="text"  :value="city"/> <!-- todo add api with all ukraine city -->
             </div>
         </div>
     </div>
 </template>
 <script>
-import NovaPoshtaService from '@/services/NovaPoshtaService' // Adjust the path based on your project structure
-
+import Inputmask from 'inputmask';
 export default {
     data() {
         return {
-            responseData: null,
+            phoneNumber: '',
+            name: '',
+            city: ''
         }
     },
+    computed: {
+        formattedPhoneNumber() {
+            return this.phoneNumber.replace(/^(\+\d{3})(\d{2})(\d{3})(\d{2})(\d{2})$/, '$1 $2 $3 $4 $5');
+        },
+    },
     mounted() {
-        // Example of using Nova Poshta API to get a list of cities
-        NovaPoshtaService.post('/common/getCities', {
-            modelName: 'Address',
-            calledMethod: 'getCities',
-            apiKey: 'fc532f0b68f563638171396db1bc8485', 
-        })
-            .then((response) => {
-                console.log(response.data)
-                this.responseData = response.data
-            })
-            .catch((error) => {
-                console.error('Error fetching data:', error)
-            })
+        this.initializeInputMask();
+    },
+    methods: {
+        initializeInputMask() { //todo repair features
+            Inputmask({
+                mask: '+380 99 999 99 99',
+                clearIncomplete: true,
+            }).mask(this.$el.querySelector('#phoneNumber'));
+        },
+        handleInput(event) {
+            if (!event.target.value.startsWith('+380')) {
+                event.target.value = '+380';
+            }
+            this.phoneNumber = event.target.value;
+        },
+        handleKeyDown(event) {
+            const cursorPosition = event.target.selectionStart;
+            if (cursorPosition <= 5 && event.key === 'Backspace') {
+                event.preventDefault();
+            }
+        },
     },
 }
 </script>
