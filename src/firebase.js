88f1c62dfab54b59f60e6e746333c56b2924f08b
Totemy <thetotemofficial@gmail.com>
2024-01-16 15:38:39 +0200

    Editing env firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,23 +3,16 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-const API_KEY = process.env.FIREBASE_API_KEY
-const AUTH_DOMAIN = process.env.FIREBASE_AUTH_DOMAIN
-const PROJECT_ID = process.env.FIREBASE_PROJECT_ID
-const STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKETT
-const MESSAGING_SENDER_ID = process.env.FIREBASE_MESSAGING_SENDER_ID
-const APP_ID = process.env.FIREBASE_APP_ID
-const MEASUREMENT_ID = process.env.FIREBASE_MEASUREMENT_ID
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID ,
-  appId: APP_ID ,
-  measurementId: MEASUREMENT_ID ,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId:  process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  process.env.FIREBASE_MASSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
