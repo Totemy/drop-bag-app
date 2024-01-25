392142b2c76b8c525d65f5aa8c08ab2ae23883c4
Totemy <thetotemofficial@gmail.com>
2024-01-25 23:41:05 +0200

    Adding category service

--- src/services/FirebaseDataService.js
+++ src/services/FirebaseDataService.js
@@ -1,5 +1,21 @@
-import { query, getDocs, collectionGroup, getFirestore } from 'firebase/firestore';
+import { query, getDocs, collectionGroup, getFirestore, doc, getDoc } from 'firebase/firestore';
 
+export async function getCategory(categoryId) {
+    const db = getFirestore();
+    const categoryRef = doc(db, 'categories', categoryId);
+    const docSnap = await getDoc(categoryRef);
+
+    if (docSnap.exists()) {
+        return {
+            id: docSnap.id,
+            name: docSnap.data().name,
+            image: docSnap.data().image,
+        };
+    } else {
+        console.log('Document does not exist.');
+        return null;
+    }
+}
 export async function getProducts(productIdToFind) {
     const db = getFirestore();
     const sousCollectionRef = query(collectionGroup(db, `products`));
